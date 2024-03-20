40e29ad0af5931fc882dabc52f4c70f8f29b354d
Anish Kumar Chaudhary <134726737+Anishkusmi@users.noreply.github.com>
2024-03-21 00:29:26 +0545

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using LaptopStoreOnline.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LaptopStoreOnline.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Laptop> laptops { get; set; }     
+    }
+}
