b887ca20c082b4b9d049b7ea5a57f9b074f2687f
Anish Kumar Chaudhary <134726737+Anishkusmi@users.noreply.github.com>
2024-03-21 00:30:09 +0545

    Add files via upload

---
+++ Laptop.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LaptopStoreOnline.Models
+{
+    public class Laptop
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name ="Brand Name")]
+        public string Brand { get; set; }
+        [Required]  
+        public string Description { get; set; }
+        public double Price { get; set; }
+        [ValidateNever]
+        public string Path { get; set; }
+        [NotMapped]
+        [Display(Name ="Choose Image")]
+        public IFormFile ImagePath { get; set; }
+
+    }
+}
