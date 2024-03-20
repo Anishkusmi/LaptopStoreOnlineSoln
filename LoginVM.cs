3d8d86c5cf6ecc2f8433fc5a96f1487faa27e429
Anish Kumar Chaudhary <134726737+Anishkusmi@users.noreply.github.com>
2024-03-21 00:31:07 +0545

    ViewModels

---
+++ LoginVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LaptopStoreOnline.ViewModels
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage ="User Name is Required")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Passroed id Required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Remeber Me")]
+        public bool RemeberMe { get; set; }
+    }
+}
